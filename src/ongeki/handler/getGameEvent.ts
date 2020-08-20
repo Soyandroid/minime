e5a5d0b9781afd5e14ec25cd19c37391940fb064
Felix <me@mbilker.us>
2020-08-20 07:41:35 +0000

    ongeki: Add event ids for option data

--- src/ongeki/handler/getGameEvent.ts
+++ src/ongeki/handler/getGameEvent.ts
@@ -2,27 +2,19 @@ import { GameEventJson } from "../proto/gameEvent";
 import { Repositories } from "../repo";
 import { GetGameEventRequest } from "../request/getGameEvent";
 import { GetGameEventResponse } from "../response/getGameEvent";
+import { EVENT_IDS } from "../static/event";
 
 export default async function getGameEvent(
   rep: Repositories,
   req: GetGameEventRequest
 ): Promise<GetGameEventResponse> {
-  const gameEventIds = [
-    1000110101,
-    1000110102,
-    1000110103,
-    1000110104,
-    1000110105,
-    1000110201,
-    1000110202,
-  ];
   const gameEventList: GameEventJson[] = [];
 
-  for (const id of gameEventIds) {
+  for (const id of EVENT_IDS) {
     gameEventList.push({
       type: req.type,
       id,
-      startDate: "0000-00-00 00:00:00.0",
+      startDate: "2018-07-26 07:00:00.0",
       endDate: "2099-12-31 00:00:00.0",
     });
   }
