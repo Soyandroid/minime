8fce611a286fe6f4416e39863cad8cb22a403ad1
Felix <me@mbilker.us>
2020-09-26 07:55:59 +0000

    ongeki: Always send first place so everyone gets every reward

--- src/ongeki/handler/getUserEventRanking.ts
+++ src/ongeki/handler/getUserEventRanking.ts
@@ -1,3 +1,5 @@
+import { readAimeId } from "../proto/base";
+import { writeUserEventRanking } from "../proto/userEventRanking";
 import { Repositories } from "../repo";
 import { GetUserEventRankingRequest } from "../request/getUserEventRanking";
 import { GetUserEventRankingResponse } from "../response/getUserEventRanking";
@@ -6,11 +8,37 @@ export default async function getUserEventRanking(
   rep: Repositories,
   req: GetUserEventRankingRequest
 ): Promise<GetUserEventRankingResponse> {
+  const aimeId = readAimeId(req.userId);
+
+  const profileId = await rep.userData().lookup(aimeId);
+  const eventPoints = await rep.userEventPoint().load(profileId);
+
+  const items = eventPoints
+    .flatMap(({ eventId, point }) => {
+      const oldDate = new Date();
+      oldDate.setDate(oldDate.getDate() - 1);
+
+      return [
+        {
+          eventId,
+          type: 0,
+          date: oldDate,
+          rank: 2,
+          point,
+        },
+        {
+          eventId,
+          type: 1,
+          date: new Date(),
+          rank: 1,
+          point,
+        },
+      ];
+    });
+
   return {
     userId: req.userId,
-    length: 0,
-    userEventRankingList: [
-      // FIXME
-    ],
+    length: items.length,
+    userEventRankingList: items.map(writeUserEventRanking),
   };
 }
