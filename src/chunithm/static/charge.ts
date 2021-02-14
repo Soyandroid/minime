8c5688905aefeac92d5b7f51b44f31f486952af7
Rob <hexagonsun657@gmail.com>
2021-02-14 02:21:50 -0600

    chuni: add other ticket types and descriptions

--- src/chunithm/static/charge.ts
+++ src/chunithm/static/charge.ts
@@ -1,16 +1,51 @@
 export const CHARGE_IDS = Object.freeze([
   {
-    id: 2310, //World's End ticket x5
+    id: 2310, //World's End ticket (x5)
     price: 1,
     salePrice: 1,
   },
+  // {
+  //   id: 2010, //1.5x bonus ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
+  // {
+  //   id: 2020, //2x bonus ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
+  // {
+  //   id: 2030, //2.5x bonus ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
+  // {
+  //   id: 2040, //3x bonus ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
+  // {
+  //   id: 2050, //3.5x bonus ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
   {
-    id: 2060, //4x bonus ticket
+    id: 2060, //4x bonus ticket (x1)
     price: 1,
     salePrice: 1,
   },
+  // {
+  //   id: 2210, //4 songs premium ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
+  // {
+  //   id: 2220, //5 songs premium ticket (x1)
+  //   price: 1,
+  //   salePrice: 1,
+  // },
   {
-    id: 2230, //6 songs premium ticket
+    id: 2230, //6 songs premium ticket (x1)
     price: 2,
     salePrice: 2,
   },
